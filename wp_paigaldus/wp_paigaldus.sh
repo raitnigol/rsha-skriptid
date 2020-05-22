93af5fbef1092c45c70082681666013f3d6201d1
Rait Nigol <rait.nigol@khk.ee>
2020-05-22 12:25:39 -0400

    uuenda wp_paigaldus.sh

--- wp_paigaldus/wp_paigaldus.sh
+++ wp_paigaldus/wp_paigaldus.sh
@@ -15,25 +15,27 @@ sh $skriptid/pma_paigaldus.sh
 wp_archive=$currentdir/latest.tar.gz
 wp_folder=$currentdir/wordpress
 apache_wp_folder=/var/www/html/wordpress
-[ -f "$wp_archive" ]
-wpa_result=$?
-if [ $wpa_result -eq 0 ]; then
-	#fail eksisteerib, pole uuesti vaja alla laadida
-	echo "Fail latest.tar.gz eksisteerib juba, jatan vahele"
-else
-	#faili ei eksisteeri, laeme alla
-	wget https://wordpress.org/latest.tar.gz
-fi
 
-if [ -d "$wp_folder" ]; then
-	#Kaust on minema liigutatud antud asukohast
-	echo "Kaust wordpress juba eksisteerib."
-elif [ -d "$apache_wp_folder" ]; then
-	# wordpress kaust eksisteerib juba /var/www/html-is
-	echo "Kaust wordpress juba eksisteerib asukohas /var/www/html"
+if [ ! -d $apache_wp_folder ]; then
+	#kontrolli, kas wordpressi kaust on olemas asukohas $currentdir/wordpress
+	if [ ! -d $wp_folder ]; then
+		#kontrolli, kas arhiiv on olemas
+		if [ -f $wp_archive ]; then
+			#arhiiv on olemas
+			echo "arhiivi latest.tar.gz juba eksisteerib"
+		else
+			wget https://wordpress.org/latest.tar.gz
+			tar xzvf latest.tar.gz
+			cp $wp_folder/wp-config-sample.php $wp_folder/wp-config.php
+			cp -r $wp_folder /var/www/html
+			echo "Wordpressi arhiiv on alla laetud."
+			echo "$wp_folder liigutatud asukohta /var/www/html/wordpress"
+		fi
+	else
+		#wordpressi kaust on olemas asukohas $currentdir/wordpress
+		echo "antud asukohas eksisteerib juba wordpress kaust."
+	fi
 else
-	#pakime arhiivi lahti
-	tar xzvf latest.tar.gz
-	cp $wp_folder/wp-config-sample.php $wp_folder/wp-config.php
-	cp -r $wp_folder /var/www/html/
+	#wordpressi kaust juba asub /var/www/html kaustas
+	echo "wordpressi kaust juba eksisteerib asukohas /var/www/html."
 fi
